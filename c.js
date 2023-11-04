8f3a852a79e607151f223a4e1ee5301b689d2eb8
sook <lenainluglio@gmail.com>
2023-11-04 13:31:15 +1300

    solution

--- c.js
+++ c.js
@@ -1,15 +1,37 @@
 // 과제5: 브라우저 변경에 따른 배경색 바꾸기
 
-console.log(window.innerWidth);
+// #submited my code
+// function handleResize() {
+//   let width = window.innerWidth;
+//   if (width < 200) {
+//     document.body.style.backgroundColor = "cornflowerblue";
+//   } else if (width >= 200 && width < 400) {
+//     document.body.style.backgroundColor = "tomato";
+//   } else if (width >= 400 && width < 600) {
+//     document.body.style.backgroundColor = "mediumpurple";
+//   }
+// }
+
+// window.addEventListener("resize", handleResize);
+
+//solution
+const body = document.body;
+
+const BIG_SCREEN = "bigScreen";
+const MEDIUM_SCREEN = "mediumScreen";
+const SMALL_SCREEN = "smallScreen";
 
 function handleResize() {
-  let width = window.innerWidth;
-  if (width < 200) {
-    document.body.style.backgroundColor = "cornflowerblue";
-  } else if (width >= 200 && width < 400) {
-    document.body.style.backgroundColor = "tomato";
-  } else if (width >= 400 && width < 600) {
-    document.body.style.backgroundColor = "mediumpurple";
+  const width = window.innerWidth;
+  if (width > 1000) {
+    body.classList.add(BIG_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  } else if (width <= 1140 && width >= 700) {
+    body.classList.add(MEDIUM_SCREEN);
+    body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
+  } else {
+    body.classList.remove(MEDIUM_SCREEN);
+    body.classList.add(SMALL_SCREEN);
   }
 }
 
